b19880c45463c7c14642d31dfbb363bc49c22f92
denisboyun <denisboyun@gmail.com>
2014-04-02 17:13:32 +0300

    Linux inotifier test stage

---
+++ FileSync/SyncDiff/Notify.pm
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+###########################################################################
+# Copyright (C) 2014  John 'Warthog9' Hawley                              #
+#                         jhawley@redhat.com                              #
+#                         warthog9@eaglescrag.net                         #
+#                                                                         #
+# This file is originally part of SyncDiff(erent).                        #
+#                                                                         #
+# This library is free software; you can redistribute it and/or           #
+# modify it under the terms of the GNU Lesser General Public              #
+# License as published by the Free Software Foundation; either            #
+# version 2.1 of the License, or (at your option) any later version.      #
+#                                                                         #
+# This library is distributed in the hope that it will be useful,         #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       #
+# Lesser General Public License for more details.                         #
+#                                                                         #
+# You should have received a copy of the GNU Lesser General Public        #
+# License along with this library; if not, write to the:                  #
+#    Free Software Foundation, Inc.                                       #
+#    51 Franklin Street                                                   #
+#    Fifth Floor                                                          #
+#    Boston, MA  02110-1301                                               #
+#    USA                                                                  #
+#                                                                         #
+# Or, see <http://www.gnu.org/licenses/>.                                 #
+###########################################################################
+
+package FileSync::SyncDiff::Notify;
+$FileSync::SyncDiff::Notify::VERSION = '0.01';
+
+use Moose;
+
+extends qw(FileSync::SyncDiff::Forkable);
+
+use Carp;
+use FileSync::SyncDiff::Notify::Plugin::Inotify2;
+use AnyEvent;
+use Data::Dumper;
+
+use MooseX::FileAttribute;
+
+has_directory 'directory' => ( must_exist => 1, required => 1);
+
+sub _load_plugin {
+    my $self = shift;
+
+    if ( $^O eq 'linux' ) {
+    	$self->_load_linux();
+    }
+
+    return 1;
+}
+
+sub load_and_fork {
+	my $self = shift;
+
+	$self->fork();
+}
+
+sub _load_linux{
+	my $self = shift;
+
+	my $cv = AnyEvent->condvar;
+
+        my $inotify = FileSync::SyncDiff::Notify::Plugin::Inotify2->new(
+        	directory => $self->directory,
+	        event_receiver => sub {
+	           my ($event, $file) = @_;
+	           print Dumper @_;
+	           if($event eq 'modify') {
+	           		print "modify file $file!\n";
+	           }
+	        },
+		);
+
+	$cv->recv;
+}
+
+override 'run_child' => sub {
+	my( $self ) = @_;
+
+	print "Run child on Notify";
+
+	$self->_load_plugin();
+};
+
+__PACKAGE__->meta->make_immutable;
